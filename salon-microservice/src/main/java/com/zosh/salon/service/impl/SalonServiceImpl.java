1c7540a9c3850ea8d9781503aa5ce73465f89ae1
K Goutham <2440552@cognizant.com>
2026-05-09 22:18:53 +0530

    updated salon microservice's service layer impl class

--- salon-microservice/src/main/java/com/zosh/salon/service/impl/SalonServiceImpl.java
+++ salon-microservice/src/main/java/com/zosh/salon/service/impl/SalonServiceImpl.java
@@ -4,10 +4,13 @@ import com.zosh.salon.entity.Salon;
 import com.zosh.salon.payload.dto.SalonDTO;
 import com.zosh.salon.payload.dto.UserDTO;
 import com.zosh.salon.service.SalonService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class Service implements SalonService {
+
+@Service
+public class SalonServiceImpl implements SalonService {
     @Override
     public Salon createSalon(SalonDTO salonDTO, UserDTO userDTO) {
         return null;
