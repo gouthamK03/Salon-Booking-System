762b64ee809c2806eebea4e4748620b6c4b0f6ff
K Goutham <2440552@cognizant.com>
2026-05-10 21:10:16 +0530

    create repository package and interface

---
+++ salon-microservice/src/main/java/com/zosh/salon/repository/SalonRepository.java
@@ -0,0 +1,19 @@
+package com.zosh.salon.repository;
+
+import com.zosh.salon.entity.Salon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SalonRepository extends JpaRepository<Salon,Long> {
+    Salon findByOwnerId(Long id);
+    @Query("select s from Salon s where " +
+            "(lower(s.city) like (lower(concat('%',:keyword,'%')) OR " +
+            "lower(s.address) like lower(concat('%',:keyword,'%')) OR " +
+            "lower(s.name) like lower(concat('%',:keyword,'%')))")
+    List<Salon> searchSalons(@Param("keyword") String keyword);
+}
