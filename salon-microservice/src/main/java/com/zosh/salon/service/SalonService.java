7f4276a720f432e1c2cd3b0c7902d0c434053597
K Goutham <2440552@cognizant.com>
2026-05-17 00:35:18 +0530

    updated SalonService methods for exception Handling

--- salon-microservice/src/main/java/com/zosh/salon/service/SalonService.java
+++ salon-microservice/src/main/java/com/zosh/salon/service/SalonService.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface SalonService {
     Salon createSalon(SalonDTO salonDTO, UserDTO userDTO);
-    Salon updateSalon(SalonDTO salonDTO,UserDTO userDTO, Long salonId);
+    Salon updateSalon(SalonDTO salonDTO,UserDTO userDTO, Long salonId) throws Exception;
     List<Salon> getAllSalons();
-    Salon getSalonById(Long salonId);
-    Salon getSalonByOwnerId(Long ownerId);
+    Salon getSalonById(Long salonId) throws Exception;
+    Salon getSalonByOwnerId(Long ownerId) throws Exception;
     List<Salon> getSalonByCity(String city);
 }
