66286e3af1c6a720cf2a52ef97319a4c362f315d
K Goutham <kgoutham834@gmail.com>
2026-05-23 01:44:17 +0530

    created Category entity in the category service

---
+++ category-service/src/main/java/com/zosh/entity/Category.java
@@ -0,0 +1,61 @@
+package com.zosh.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public String name;
+
+    public String image;
+
+    @Column(nullable = false)
+    public Long salonId;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String image, Long salonId) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.salonId = salonId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getSalonId() {
+        return salonId;
+    }
+
+    public void setSalonId(Long salonId) {
+        this.salonId = salonId;
+    }
+}
