1ccb2bdf7a8c1e0838bd7a1485e894a96249d428
K Goutham <kgoutham834@gmail.com>
2026-05-23 20:38:34 +0530

    created the CategoryController and added endpoints

---
+++ category-service/src/main/java/com/zosh/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.zosh.controller;
+
+import com.zosh.dto.SalonDTO;
+import com.zosh.entity.Category;
+import com.zosh.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+public class CategoryController {
+
+    private final CategoryService categoryService;
+    @Autowired
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/api/category/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id){
+        return new ResponseEntity<>(categoryService.getCategoryById(id),HttpStatus.OK);
+    }
+
+    @GetMapping("/api/category/salonId/{salonId}")
+    public ResponseEntity<Set<Category>> getCategoryBySalonId(@PathVariable Long salonId){
+        return new ResponseEntity<>(categoryService.getAllCategoryBySalon(salonId),HttpStatus.OK);
+    }
+
+}
