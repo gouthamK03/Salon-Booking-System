168d6090b834b46e580794480ebf0f00be6eddd5
K Goutham <kgoutham834@gmail.com>
2026-05-23 22:58:38 +0530

    updated home controller of category microservice

--- category-service/src/main/java/com/category/controller/HomeController.java
+++ category-service/src/main/java/com/category/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.category.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
 
     @GetMapping
     public String HomeControllerHandler(){
+
         return "Welcome to category microservice of Salon Booking System";
     }
 }
