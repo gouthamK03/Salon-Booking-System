4b4e0f0850260815687520d60a47e56be68a3f55
K Goutham <kgoutham834@gmail.com>
2026-05-24 16:26:40 +0530

    add CategoryDTO in serviceOffering

---
+++ service-offering/src/main/java/com/zosh/dto/CategoryDTO.java
@@ -0,0 +1,40 @@
+package com.zosh.dto;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String image;
+
+    public CategoryDTO() {
+    }
+
+    public CategoryDTO(String name, Long id, String image) {
+        this.name = name;
+        this.id = id;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
