431729448cdedb8a58efa340feb8fa52fdc5e829
K Goutham <kgoutham834@gmail.com>
2026-05-24 19:12:12 +0530

    updated ServiceOfferingService

--- service-offering/src/main/java/com/zosh/service/ServiceOfferingService.java
+++ service-offering/src/main/java/com/zosh/service/ServiceOfferingService.java
@@ -1,10 +1,16 @@
 package com.zosh.service;
 
+import com.zosh.dto.CategoryDTO;
 import com.zosh.dto.SalonDTO;
 import com.zosh.dto.ServiceDTO;
 import com.zosh.entity.ServiceOffering;
 
+import java.util.Set;
+
 public interface ServiceOfferingService {
 
     ServiceOffering createService(SalonDTO salonDTO, ServiceDTO serviceDto, CategoryDTO categoryDto);
+    ServiceOffering updateService(Long id,ServiceOffering serviceOffering);
+    Set<ServiceOffering> getServiceOfferingsBySalonId(Long salonId, Long categoryId);
+    Set<ServiceOffering> getServicesByIds(Set<Long>ids);
 }
