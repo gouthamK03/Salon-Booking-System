ca2ddffaaf53a3a3c4ad30066628fcaf693b2c80
K Goutham <kgoutham834@gmail.com>
2026-05-30 01:33:14 +0530

    added some DTOs

---
+++ booking-service/src/main/java/com/zosh/booking_service/dto/SalonDTO.java
@@ -0,0 +1,123 @@
+package com.zosh.booking_service.dto;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class SalonDTO {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private List<String> images;
+
+    private String phone;
+
+    private String address;
+
+    private String city;
+
+    private LocalTime open;
+    private LocalTime close;
+    private Long ownerId;
+    public SalonDTO() {
+    }
+
+
+    public SalonDTO(Long id, String name, String email,
+                    List<String> images, String phone,
+                    String address, LocalTime open,
+                    String city, LocalTime close, Long ownerId) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.images = images;
+        this.phone = phone;
+        this.address = address;
+        this.open = open;
+        this.city = city;
+        this.close = close;
+        this.ownerId = ownerId;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalTime getOpen() {
+        return open;
+    }
+
+    public void setOpen(LocalTime open) {
+        this.open = open;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public LocalTime getClose() {
+        return close;
+    }
+
+    public void setClose(LocalTime close) {
+        this.close = close;
+    }
+}
---
+++ booking-service/src/main/java/com/zosh/booking_service/dto/ServiceDTO.java
@@ -0,0 +1,92 @@
+package com.zosh.booking_service.dto;
+
+public class ServiceDTO {
+    private Long id;
+    private String name;
+    private String image;
+    private String description;
+    private int price;
+    private int duration;
+    private Long salonId;
+    private Long category;
+
+    public ServiceDTO() {
+    }
+
+    public ServiceDTO(Long id, String name, String image,
+                      String description, int price,
+                      int duration, Long salonId, Long category) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.price = price;
+        this.duration = duration;
+        this.salonId = salonId;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Long getSalonId() {
+        return salonId;
+    }
+
+    public void setSalonId(Long salonId) {
+        this.salonId = salonId;
+    }
+
+    public Long getCategory() {
+        return category;
+    }
+
+    public void setCategory(Long category) {
+        this.category = category;
+    }
+}
